af6538784229877f9a24ab5cf0054fec3749cd60
Dery Galeas <dery.reyna@gmail.com>
2023-07-15 12:25:09 -0600

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { url } from 'inspector';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname
+    const isPublic = path === "/login" || path === '/signup'
+
+    const token = request.cookies.get('token')?.value || '';
+
+    if(isPublic && token){
+        return NextResponse.redirect(new URL('/', request.nextUrl))
+    }
+
+    if(!isPublic && !token){
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+}
+ 
+
+export const config = {
+  matcher: [
+    "/",
+    "/profile",
+    "/login",
+    "/logout",
+    "/signup",
+  ]
+}
